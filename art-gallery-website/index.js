c7e178ea8654f03bffb7fd9996caa0cfbecc2da5
Gideon Onita <onitagideon30@gmail.com>
2024-02-29 16:21:53 +0100

    completed

--- art-gallery-website/index.js
+++ art-gallery-website/index.js
@@ -1,9 +1,8 @@
-const map = L.map('map').setView([51.505, -0.09], 13)
+const map = L.map('map').setView([6.42387, 3.4209], 13)
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
   attribution:
     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map)
 
-const marker = L.marker([51.5, -0.09]).addTo(map)
-marker.bindPopup('<b>Hello world!</b><br>I am a popup.').openPopup()
+const marker = L.marker([6.42387, 3.4209]).addTo(map)
